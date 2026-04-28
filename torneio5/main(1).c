b9872d452c7287c0c8c1564aef01aa8156367d7c
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-04-28 07:36:19 -0300

    Add files via upload

---
+++ torneio5/main(1).c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int t;
+    
+    // Lê a quantidade de casos de teste
+    if (scanf("%d", &t) != 1) {
+        return 0;
+    }
+
+    while (t--) {
+        char str[60];
+        int n;
+
+        // Lê a string codificada e o número de deslocamento
+        scanf("%s", str);
+        scanf("%d", &n);
+
+        for (int i = 0; str[i] != '\0'; i++) {
+            // Converte char para número (A=0, B=1, ..., Z=25)
+            int val = str[i] - 'A';
+
+            // Decodifica: subtrai n
+            val = val - n;
+
+            // Ajuste para números negativos (wrap-around)
+            // Em C, o módulo de número negativo pode ser negativo.
+            // Adicionamos 26 até ficar positivo.
+            while (val < 0) {
+                val += 26;
+            }
+            
+            // Garante que está dentro de 0-25 (caso val >= 26, embora a lógica acima cubra)
+            val = val % 26;
+
+            // Converte de volta para char
+            str[i] = 'A' + val;
+        }
+
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
