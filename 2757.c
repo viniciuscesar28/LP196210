d05744b7c418b62d121990db266d686004635a61
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-04-30 00:24:36 -0300

    Update 2757.c

--- 2757.c
+++ 2757.c
@@ -1,4 +1,12 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1013
+Data        : 07/04/2026
+Objetivo    : Fazera um programa que crie 3 variaveis(com os valores dentro dos parametros indicados na questão) e as imprima de 4 formas (formas especificadas na questão)
+Aprendizado : <<<Uso do scanf>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
