242a18b935c380fb481db1e22209e509bdb5f5f9
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-04-30 00:07:59 -0300

    Update and rename torneio1/Hello World! to 1000.c

---
+++ 1000.c
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 07/04/2026
+Objetivo    : imprimir a menssagem "Hello World" na tela
+Aprendizado : <<<Foi aprendido noções basica de C>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
