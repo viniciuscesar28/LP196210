79a6180d199445639b15ca2a57ba85803b4b83ac
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-07 14:27:21 -0300

    Update 1241.c

--- 1241.c
+++ 1241.c
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
-Linguagem   : C
+Linguagem   : C99
 Problema    : https://judge.beecrowd.com/pt/problems/view/1241
 Data        : 05/05/2026
 Objetivo    : Fazer um programa para verificar, à partir de dois valores muito grandes A e B, se B corresponde aos últimos dígitos de A.
