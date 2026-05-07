56b90cf9db05d87e33feadd5d03d6b3630448d24
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-07 14:09:01 -0300

    Create 1024.c

---
+++ 1024.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    getchar(); // consumir '\n'
+
+    char str[1005];
+
+    while (N--) {
+        fgets(str, sizeof(str), stdin);
+
+        int len = strlen(str);
+
+        // remover '\n' do fgets
+        if (str[len - 1] == '\n') {
+            str[len - 1] = '\0';
+            len--;
+        }
+
+        // 1ª passada: desloca letras +3
+        for (int i = 0; i < len; i++) {
+            if ((str[i] >= 'a' && str[i] <= 'z') ||
+                (str[i] >= 'A' && str[i] <= 'Z')) {
+                str[i] += 3;
+            }
+        }
+
+        // 2ª passada: inverter string
+        for (int i = 0; i < len / 2; i++) {
+            char temp = str[i];
+            str[i] = str[len - 1 - i];
+            str[len - 1 - i] = temp;
+        }
+
+        // 3ª passada: metade em diante -1
+        for (int i = len / 2; i < len; i++) {
+            str[i] -= 1;
+        }
+
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
