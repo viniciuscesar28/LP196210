94cf29cea0954836ad37364c57c6ac1ad7826226
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-07 14:27:33 -0300

    Update 1253.c

--- 1253.c
+++ 1253.c
@@ -1,7 +1,7 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
-Linguagem   : C
+Linguagem   : C99
 Problema    : https://judge.beecrowd.com/pt/problems/view/1253
 Data        : 23/04/2026
 Objetivo    : Fazer uma cifra de cezar
