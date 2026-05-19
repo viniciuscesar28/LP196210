973087d65120876cb410c85e733514a53828003c
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-19 20:55:19 -0300

    Update 1046.C

--- 1046.C
+++ 1046.C
@@ -1,4 +1,12 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C99
+Problema    : https://judge.beecrowd.com/pt/problems/view/1046
+Data        : 19/05/2026
+Objetivo    : fazer um programa que leia a hora de iniciode um jogo e dê sua duração, sabendo que pode começar em um dia e acabr no seguinte, com duração mínima de 1h e máxima de 24.
+Aprendizado : <<<praticar c>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 // Função para calcular a duração do jogo
