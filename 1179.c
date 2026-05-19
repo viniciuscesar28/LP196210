3c1aadc19ea6ddb351622a34e31077fbbd07a7ee
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-19 20:57:12 -0300

    Create 1179.c

---
+++ 1179.c
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C99
+Problema    : https://judge.beecrowd.com/pt/problems/view/1179
+Data        : 19/05/2026
+Objetivo    : 
+Aprendizado : <<<praticar c>>>
+-------------------------------------------------------------------------- */
