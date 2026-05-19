bd8d14f37ae5ffcd8163ed3d3f5f9f6674c060dd
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-19 20:19:13 -0300

    Create 1534.c

---
+++ 1534.c
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C99
+Problema    : https://judge.beecrowd.com/pt/problems/view/1534
+Data        : 19/05/2026
+Objetivo    : imprimir a menssagem "Hello World" na tela
+Aprendizado : <<<Foi aprendido noções basica de C>>>
+-------------------------------------------------------------------------- */
