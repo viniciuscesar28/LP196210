6e84b26183bcd56c1679ce47d84c402cd3f8b780
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-19 20:46:01 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -1,4 +1,12 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C99
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 19/05/2026
+Objetivo    : obter a distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostrando o valor com 3 casas decimais após o ponto.
+Aprendizado : <<<praticar c>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 // Função para calcular litros de combustível
