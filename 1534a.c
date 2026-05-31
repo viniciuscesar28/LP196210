571a8e606a27662459f835cc775a5c8e30637a82
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-31 01:28:38 -0300

    Update and rename 1534.c to 1534a.c

--- 1534a.c
+++ 1534a.c
@@ -7,7 +7,6 @@ Data        : 19/05/2026
 Objetivo    : Ler um valor inteiro N que é o tamanho da matriz que deve ser impressa conforme o modelo fornecido.
 Aprendizado : <<<pratica de c>>>
 -------------------------------------------------------------------------- */
-verssão normal:
 #include <stdio.h>
 
 int main() {
@@ -23,28 +22,6 @@ int main() {
                 } else {
                     printf("3");
                 }
-            }
-            printf("\n");
-        }
-    }
-    
-    return 0;
-}
-verssão sem operador:
-#include <stdio.h>
-
-int main(void) {
-    int N;
-    
-    while (scanf("%d", &N) != EOF) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                int dp = (i == j);          // 1 se diagonal principal, 0 caso contrário
-                int ds = (i + j == N - 1);  // 1 se diagonal secundária, 0 caso contrário
-                
-                // Prioridade: dp(1) > ds(2) > resto(3)
-                int val = dp * 1 + (1 - dp) * ds * 2 + (1 - dp) * (1 - ds) * 3;
-                printf("%d", val);
             }
             printf("\n");
         }
