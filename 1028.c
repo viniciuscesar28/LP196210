557afd9207a8c6bcf515698490980ec391baed81
Vinicius Cesar Sotelo Ambires <vinicius.cesar28@unifesp.br>
2026-05-31 01:00:16 -0300

    Update 1028.c

--- 1028.c
+++ 1028.c
@@ -1,4 +1,12 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Vinícius Cesar Sotelo Ambires>>>
+Linguagem   : C99
+Problema    : https://judge.beecrowd.com/pt/problems/view/1028
+Data        : 26/05/2026
+Objetivo    : calcular o Máximo Divisor Comum (MDC) entre dois números, que representa o maior tamanho de pilha possível que pode ser formado igualmente para ambos os jogadores.
+Aprendizado : <<<pratica c e usar recursão>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int mdc(int a, int b) {
